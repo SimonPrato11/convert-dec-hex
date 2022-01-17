a65bfc18164ad2692517029124a512c703c69bd8
Simon Prato <simonprato11@gmail.com>
2022-01-17 17:54:51 +0000

    installing heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./port/index.js
--- port/index.js
+++ port/index.js
@@ -2,6 +2,6 @@ import app from '../index.js'
 
 const PORT = 3000
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT, function () {
   console.log('PORT 3000 UP AND RUNNING')
 })
